7677655299f6964e7246e3bffd5d11449df0bd6c
Papp Krisztián <fejlesztes@letscode.hu>
2016-11-13 12:14:39 +0100

    jQuery/Bootstrap added

--- src/HtmlRenderer.php
+++ src/HtmlRenderer.php
@@ -11,15 +11,17 @@ namespace Letscodehu\HumbugHtml;
 
 class HtmlRenderer implements Renderer {
 
-    private $outDir;
+    private $outDir, $resourcesDir;
 
     public function __construct($outDir = "humbugreports") {
         $this->outDir = $outDir;
+        $this->resourcesDir = getcwd().DS.$outDir.DS."resources".DS;
     }
 
     public function render(Project $project)
     {
         $this->createDirectories();
+        $this->copyResources();
         $this->renderProjectView($project);
         $this->renderFileViews($project);
         $this->renderMutantViews($project);
@@ -28,21 +30,31 @@ class HtmlRenderer implements Renderer {
 
     private function renderProjectView(Project $project) {
         ob_start();
-        include "stubs".DIRECTORY_SEPARATOR."project.phtml";
+        include "stubs".DS."project.phtml";
         $content = ob_get_contents();
         ob_end_clean();
-        file_put_contents($this->outDir. DIRECTORY_SEPARATOR . "index.html", $content);
+        file_put_contents($this->outDir. DS . "index.html", $content);
     }
 
     private function renderFileViews(Project $project) {
         /** @var File $file */
         foreach ($project->getFiles() as $file) {
             ob_start();
-            include "stubs".DIRECTORY_SEPARATOR."file.phtml";
+            include "stubs".DS."file.phtml";
             $content = ob_get_contents();
             ob_end_clean();
-            $this->forceFilePutContents($this->outDir. DIRECTORY_SEPARATOR . "files". DIRECTORY_SEPARATOR. $file->getName().".html", $content);
+            $this->forceFilePutContents($this->outDir. DS . "files". DS. $file->getName().".html", $content);
         }
+    }
+
+    private function copyResources() {
+        copy(LIBRARY_ROOT.DS."resources".DS."style.css", $this->resourcesDir."style.css");
+        copy(getcwd().DS."vendor".DS."twbs".DS."bootstrap".DS."dist".DS."css".DS."bootstrap.min.css",
+            $this->resourcesDir."bootstrap.min.css");
+        copy(getcwd().DS."vendor".DS."twbs".DS."bootstrap".DS."dist".DS."js".DS."bootstrap.min.js",
+            $this->resourcesDir."bootstrap.min.js");
+        copy(getcwd().DS."vendor".DS."components".DS."jquery".DS."jquery.min.js",
+            $this->resourcesDir."jquery.min.js");
     }
 
     function forceFilePutContents ($filepath, $content){
@@ -68,6 +80,7 @@ class HtmlRenderer implements Renderer {
         $this->createDirectory();
         $this->createDirectory("files");
         $this->createDirectory("errored");
+        $this->createDirectory("resources");
         $this->createDirectory("uncovered");
         $this->createDirectory("killed");
         $this->createDirectory("timeouts");
@@ -75,8 +88,8 @@ class HtmlRenderer implements Renderer {
     }
 
     private function createDirectory($suffix = "") {
-        if (!is_dir($this->outDir.DIRECTORY_SEPARATOR.$suffix)) {
-            mkdir($this->outDir.DIRECTORY_SEPARATOR.$suffix);
+        if (!is_dir($this->outDir.DS.$suffix)) {
+            mkdir($this->outDir.DS.$suffix);
         }
     }
 
